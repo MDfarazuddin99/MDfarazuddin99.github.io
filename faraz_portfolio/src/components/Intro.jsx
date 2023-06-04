845c2937df2da36ac47ace80ab63e57568e8cdf5
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2023-06-03 21:22:46 -0700

    Portfolio Component Done

---
+++ faraz_portfolio/src/components/Intro.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Farazuddin Mohammad</h1>
+      <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer & Full Stack Developer</p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+            I'm an indie full-stack developer and content creator building my
+            version of the digital world one step at a time. All coding projects
+            are built from the ground up, from planning and designing all the way
+            to solving real-life problems with code.
+            <br />
+            
+            My github Link is{' '}
+            <a
+               href="https://github.com/MDfarazuddin99"
+               target="_blank"
+               className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
+               rel="noreferrer noopener"
+            >
+               Farazuddin Mohammad
+            </a>{' '}
+            to more than 400k subscribers.
+         </p>
+    </div>
+  )
+}
+
+export default Intro
---
+++ faraz_portfolio/src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'project-1',
+        imgUrl: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80',
+        stack: ["HTML", 'CSS', "JavaScript"],
+        link: 'github.com/MDfarazuddin99'
+    },
+    {
+        title: 'project-2',
+        imgUrl: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80',
+        stack: ["HTML", 'CSS', "JavaScript"],
+        link: 'github.com/MDfarazuddin99'
+    },
+    {
+        title: 'project-3',
+        imgUrl: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80',
+        stack: ["HTML", 'CSS', "JavaScript"],
+        link: 'github.com/MDfarazuddin99'
+    }
+]
