e5a62a5784705e19279c783decc8460b030a7b61
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2023-06-04 13:37:48 -0700

    Timeline Done

---
+++ faraz_portfolio/src/data/timeline.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        year: '2022',
+        title: "Master's Degreen in Software Engineering" ,
+        duration: 'Graduate Education',
+        details:
+            "Course Work: Software Systems Engineering, Cloud Technologies, Virtualization Technologies.",
+    },
+    {
+         year: '2021',
+         title: 'DevOps Engineer at Deloitte',
+         duration: '1.5 years',
+         details:
+             "Deployed Applications and built Azure cloud infrastructure for COTS software (DNAV) by adopting DevOps Practices such as CI, CD, and blue-green deployment in runtime with Azure DevOps, Git, Jenkins, Ansible, Kubernetes, Docker. Provisioned 200+ Storage Accounts, 50+ Virtual Machines, 50+ Load Balancers; maintained Kubernetes(K8s) cluster with Azure Container Service (ACS). Coded automation framework for provisioning and monitoring Docker based microservices with 30+ PowerShell Scripts and 20+ YAML pipelines, developed 100+ Azure workbooks using automation scripts and YAML pipelines to monitor and debug issues. Configured 100+ security groups and set inbound Outbound Rules giving all ports and network protocols a secure access to VMs, Redis, Cosmos DB and MySQL.",
+     },
+     {
+         year: '2020',
+         title: 'Software Engineer Intern at Ericsson',
+         duration: '6 months',
+         details:
+             'Designed and enhanced invoice querying microservice using GraphQL and NodeJS, increasing efficiency by 40%. Performed load testing using Jmeter; created test plans and simulated realistic load scenarios. Coded 10+ RESTful services for extracting structured data from massive XML file with Xpath queries. Executed and tested machine learning models for detecting malicious invoices; performed necessary feature engineering improving accuracy by 15%; performed root cause analysis using SHAP API.',
+     },
+     {
+         year: '2017',
+         title: "Bachelor's Degree in Computer Science",
+         duration: '4 years',
+         details: 'Bachelors from India in Computer Science',
+     },
+
+ ]
