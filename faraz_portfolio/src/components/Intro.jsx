6b83dfb8421df57290ca27f0d9cba1cdcc20b66f
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2023-10-15 02:42:36 -0700

    edited intro

--- faraz_portfolio/src/components/Intro.jsx
+++ faraz_portfolio/src/components/Intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
       <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Farazuddin Mohammad 🧑🏽‍💻</h1>
       <p className='text-base md:text-xl mb-3 font-medium'>DevOps + Full Stack Engineer </p>
       <p className="text-md max-w-xl mb-6" >
-              I'm an Full Stack Software Engineer, with a keen focus on DevOps and Backend Web Development, proudly based in sunny San Jose, California. My passion lies in automating manual tasks using the power of Python, crafting dynamic websites with JavaScript, and conquering algorithmic challenges in Java.
+              I'm a Full Stack Software Engineer, with a keen focus on DevOps and Backend Web Development, proudly based in sunny San Jose, California. My passion lies in automating manual tasks using the power of Python, crafting dynamic websites with JavaScript, and conquering algorithmic challenges in Java.
               <br /> <br/>
 
               I bring to the table a year of hands-on experience as a DevOps Engineer, and I've also spent six months as a software engineering intern, all while I continue my journey as a student at the San Jose State University (Go Spartans!). My heart beats for technology, and I can't wait to take on the next exciting challenge that comes my way!
